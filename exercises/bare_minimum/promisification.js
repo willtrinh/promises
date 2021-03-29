9b30ce1ae270873e568b4898151b40b2a1c18864
Eric Vogler <ericvogler@gmail.com>
2021-03-29 15:22:04 -0700

    Implement functions in promiseConstructor

--- exercises/bare_minimum/promisification.js
+++ exercises/bare_minimum/promisification.js
@@ -29,7 +29,7 @@ var getGitHubProfile = function (user, callback) {
   });
 };
 
-var getGitHubProfileAsync = Promise.promisify(getGitHubProfile); // TODO
+var getGitHubProfileAsync = Promise.promisify(getGitHubProfile);
 
 
 // (2) Asyncronous token generation
@@ -40,7 +40,7 @@ var generateRandomToken = function(callback) {
   });
 };
 
-var generateRandomTokenAsync; // TODO
+var generateRandomTokenAsync = Promise.promisify(generateRandomToken);
 
 
 // (3) Asyncronous file manipulation
