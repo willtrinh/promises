db31b092cdf45864ac282669d15096c438033e9f
Eric Vogler <ericvogler@gmail.com>
2021-03-30 10:05:53 -0700

    Implement searchCommonConceptsFromGitHubProfiles (without chainging)

--- exercises/advanced/advancedChaining.js
+++ exercises/advanced/advancedChaining.js
@@ -5,8 +5,8 @@
  *
  * Given an array of github handles, searchCommonConceptsFromGitHubProfiles should:
  *   1) get the public profile associated with each handle
- *   2) extract the avatar_url of each profile
- *   3) get the set of concepts for each avatar_url (requires API key)
+ *   2) extract the avatarUrl of each profile
+ *   3) get the set of concepts for each avatarUrl (requires API key)
  *   4) find the intersection of the concepts
  *
  * Much of the heavy lifting has been done already in `lib/advancedChainingHelpers`,
@@ -29,10 +29,17 @@ var lib = require('../../lib/advancedChainingLib');
 // is ready, copy and add it to the `advancedChainingLib.js` file.
 
 var searchCommonConceptsFromGitHubProfiles = function (githubHandles) {
-  Promise.all(githubHandles.map(githubHandle => lib.getGitHubProfile(githubHandle))
-    .then((users) => Promise.resolve(
-      users.map((user) => user.avatar_url)))
-    .then((avatarUrls) => avatarUrls.map((url) => lib.predictImage(url))));
+  let p = Promise.all(githubHandles.map(githubHandle => lib.getGitHubProfile(githubHandle)));
+
+  p = p.then((users) => users.map((user) => user.avatarUrl));
+
+  p = p.then((avatarUrls) => avatarUrls.map((url) => lib.predictImage(url)));
+
+  // p.then(x => Promise.all(x).then(x => console.log('*****', x)));
+
+  p = Promise.all(p);
+
+  return p.then(lib.getIntersection);
 };
 
 // Export these functions so we can unit test them
